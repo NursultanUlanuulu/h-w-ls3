5a207aea48acabefd0a199967b333f216f907c78
unknown <ulanuulunursultan36@gmail.com>
2024-09-10 20:53:24 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,76 @@
+// Условные операторы if else , swich case
+// типы данных
+// 8 тип данных бар
+// string
+// number
+// boolean
+// null
+// undefined
+// object
+// simbol
+// bigInt
+
+// const num = parseInt(prompt('Сан жаз'))
+// const num2 = parseInt(prompt('Дагы сан жаз'))
+// if (num === num2) {
+// 	console.log('cандар барабар')
+// } else if (num > num2) {
+// 	console.log(`${num} ${num2}дон чон`)
+// } else if (num < num2) {
+// 	console.log(`${num2} ${num} чон`)
+// } else {
+// 	console.log('кыймыл аткарылган жок')
+// }
+
+// switch ('выражение') {
+// 	case 'значение1':
+// 		break
+// 	case 'значение':
+// 		break
+// 	default:
+// 		console.log('кейстер аткарылган жок')
+// }
+
+// let day = parseInt(prompt('канчанчы кун'))
+
+// switch (day) {
+// 	case 'банан':
+// 		console.log('понедельник')
+// 		break
+// 	case 2:
+// 		console.log('вторник')
+// 		break
+// 	case 3:
+// 		console.log('среда')
+// 		break
+// 	case 4:
+// 		console.log('четверг')
+// 		break
+// 	case 5:
+// 		console.log('пятница')
+// 		break
+// 	case 6:
+// 		console.log('суббота')
+// 		break
+// 	case 7:
+// 		console.log('воскресенье')
+// 		break
+// 	default:
+// 		console.log('мындай кун жок')
+// }
+// введение в массивы  и объекты
+// [] , {}
+const array = ['text', 1, false, null, undefined, 'text', 4, 5, 5, { key: 1 }]
+
+console.log(array)
+console.log(array[5])
+console.log(array[6])
+
+const obj = {
+	key: 'text',
+	key2: 2,
+	key3: false,
+}
+
+console.log(obj)
+console.log(obj.key3)
